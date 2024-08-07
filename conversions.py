e4eb78199d548f394c2d41fcd2db3bb3350ccb36
Mateusz Krzykowski <matkrzykowski@gmail.com>
2024-08-08 00:44:57 +0200

    Move expr_to_float to conversions

--- conversions.py
+++ conversions.py
@@ -1,9 +1,28 @@
 "conversions.py"
 
 import datetime as dt
+
+import numexpr
 import pandas as pd
 
 
 def str_to_datetime(time_str: str) -> dt.datetime:
     """Converts string to datetime object using pandas."""
     return pd.to_datetime(time_str).to_pydatetime()
+
+
+def expr_to_float(x: str | float) -> float:
+    """Converts a simple expression (even simply a number) into a number.
+
+    Args:
+        x (str | float): A string expression or a number.
+
+    Returns:
+        float: Result of the expression.
+    """
+    match x:
+        case float():
+            return x
+        case str():
+            return float(+numexpr.evaluate(x))
+    return float(x)
--- input_output.py
+++ input_output.py
@@ -6,24 +6,6 @@ import shutil
 import tomllib
 
 import numpy as np
-import numexpr
-
-
-def expr_to_float(x: str | float) -> float:
-    """Converts a simple expression (even simply a number) into a number.
-
-    Args:
-        x (str | float): A string expression or a number.
-
-    Returns:
-        float: Result of the expression.
-    """
-    match x:
-        case float():
-            return x
-        case str():
-            return float(+numexpr.evaluate(x))
-    return float(x)
 
 
 def pretty_float(x: float, max_digits: int = 5) -> str:
